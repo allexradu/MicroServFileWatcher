7f99686e8ee87e53dac7630eea7a491b50566a2d
allexradu <NeOac/\sa666H()mE>
2022-03-05 18:50:42 +0200

    finishing the software

---
+++ src/events.py
@@ -0,0 +1,143 @@
+import os
+import pathlib
+from os import walk
+from watchdog.events import RegexMatchingEventHandler
+from enum import Enum
+import re
+from shutil import copyfile, rmtree
+from sys import exit
+import sys
+import time
+
+
+class EventType(Enum):
+    ON_CREATED = 'ON_CREATED'
+    ON_MODIFIED = 'ON_MODIFIED'
+    ON_MOVED = 'ON_MOVED'
+    ON_DELETED = 'ON_DELETED'
+
+
+class FileEventHandler(RegexMatchingEventHandler):
+
+    def __init__(self, regex, sub_folders, buffer_time):
+        self.__regex = regex
+        self.__sub_folders = sub_folders
+        self.__time = time.time()
+        self.__first_run = True
+        self.__slash = "\\" if sys.platform == 'win32' else '/'
+        self.__buffer_time = buffer_time
+        super().__init__(self.__regex)
+
+    def on_created(self, event):
+        self.process(event, EventType.ON_CREATED)
+
+    def on_modified(self, event):
+        self.process(event, EventType.ON_MODIFIED)
+
+    def on_moved(self, event):
+        self.process(event, EventType.ON_MOVED)
+
+    def on_deleted(self, event):
+        self.process(event, EventType.ON_DELETED)
+
+    def check_time(self):
+        if time.time() - self.__time >= self.__buffer_time:
+            self.__time = time.time()
+            return True
+        else:
+            return False
+
+    def copy_files(self, event, current_folder, pre, post, moved = False):
+        if self.check_time() or self.__first_run or moved:
+            self.__first_run = False
+            for sub_folder in self.__sub_folders:
+                if sub_folder != current_folder:
+                    new_file = os.path.join(pre, sub_folder, post)
+                    folder = f'{self.__slash}'.join(new_file.split(self.__slash)[:-1])
+                    try:
+                        os.makedirs(folder)
+                    except FileExistsError:
+                        pass
+
+                    try:
+                        path = event.dest_path if moved else event.src_path
+                        print('COPY FILE: ' + path + ' -> ' + new_file)
+                        copyfile(path, new_file)
+                    except IOError as e:
+                        print("Unable to copy file. %s" % e)
+                        exit(1)
+                    except:
+                        print("Unexpected error:", sys.exc_info())
+                        exit(1)
+
+    def delete_files(self, event, current_source_folder, pre, post):
+        if self.check_time() or self.__first_run:
+            self.__first_run = False
+            for sub_folder in self.__sub_folders:
+                new_file = os.path.join(pre, sub_folder, post)
+                if sub_folder != current_source_folder:
+                    folder = f'{self.__slash}'.join(new_file.split(self.__slash)[:-1])
+
+                    try:
+                        print('DELETE FILE: ' + event.src_path + ' -> ' + new_file)
+                        os.remove(new_file)
+                    except OSError as e:
+                        print("Error: %s - %s." % (e.filename, e.strerror))
+
+                    filenames = next(walk(folder), (None, None, []))[2]
+                    print(filenames)
+                    if not filenames:
+                        try:
+                            rmtree(folder)
+                        except (Exception,) as err:
+                            print('cannot delete folder: ', folder, err)
+                            pass
+
+    def find_index(self, split_path):
+        if split_path:
+            for part in split_path:
+                for folder in self.__sub_folders:
+                    if part == folder:
+                        return split_path.index(part)
+
+    def process(self, event, event_type):
+        source_file_split = event.src_path.split(self.__slash)
+
+        filename, ext = os.path.splitext(event.src_path)
+        source_index = self.find_index(source_file_split)
+        current_source_folder = source_file_split[source_index]
+
+        pre = f'{self.__slash}'.join(source_file_split[:source_index])
+        post = f'{self.__slash}'.join(source_file_split[source_index + 1:])
+
+        if ext[-1] != '~':
+            match event_type:
+                case EventType.ON_CREATED:
+                    print('1. ON_CREATED', event.src_path)
+                    self.copy_files(event, current_source_folder, pre, post)
+                case EventType.ON_MODIFIED:
+                    print('2. ON_MODIFIED', event.src_path)
+                    self.copy_files(event, current_source_folder, pre, post)
+
+                case EventType.ON_MOVED:
+                    pattern = re.compile(self.__regex[0])
+                    dest_file_split = event.dest_path.split(self.__slash)
+                    dest_matched = pattern.match(event.dest_path)
+                    source_matched = pattern.match(event.src_path)
+                    dest_index = self.find_index(dest_file_split)
+                    current_dest_folder = dest_file_split[source_index]
+                    dest_pre = f'{self.__slash}'.join(dest_file_split[:dest_index])
+                    dest_post = f'{self.__slash}'.join(dest_file_split[dest_index + 1:])
+
+                    print('3. ON_MOVED', event.src_path, event.dest_path)
+                    if source_matched:
+                        self.delete_files(event, current_source_folder, pre, post)
+                    if dest_matched:
+                        self.copy_files(event, current_dest_folder, dest_pre, dest_post, True)
+
+                    print('source_matched', source_matched)
+                    print('dest_matched', dest_matched)
+                    pass
+                case EventType.ON_DELETED:
+                    print('4. ON_DELETED', event.src_path)
+                    self.delete_files(event, current_source_folder, pre, post)
---
+++ src/main.py
@@ -0,0 +1,68 @@
+import sys
+import time
+
+from watchdog.observers import Observer
+from events import FileEventHandler
+
+
+class FileWatcher:
+    def __init__(self, file_src_path, regex, sub_folders, buffer_time):
+        self.__src_path = file_src_path
+        self.__event_handler = FileEventHandler(regex, sub_folders, buffer_time)
+        self.__event_observer = Observer()
+
+    def run(self, ):
+        self.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.stop()
+
+    def start(self):
+        self.__schedule()
+        self.__event_observer.start()
+
+    def stop(self):
+        self.__event_observer.stop()
+        self.__event_observer.join()
+
+    def __schedule(self):
+        self.__event_observer.schedule(
+            self.__event_handler,
+            self.__src_path,
+            recursive = True
+        )
+
+
+if __name__ == "__main__":
+
+    def execute():
+        BUFFER_TIME = 10
+        if len(sys.argv) < 5 or len(sys.argv) > 5:
+            return print(f"Error: Run MicroSRV FileWatcher with 3 arguments. Current arguments: {len(sys.argv) - 1}")
+        common_folder = sys.argv[1]
+        print('Common Folder:', common_folder)
+        file_extensions = sys.argv[2].split('|')
+        print('File Extensions:', file_extensions)
+        sub_folders = sys.argv[3].split('|')
+        print('Sub folders:', sub_folders)
+
+        regex_extensions = []
+        for ext in file_extensions:
+            reg = f"({'|'.join(sub_folders)})" + r"\/" + common_folder + r"\/.*\." + ext
+            regex_extensions.append(reg)
+        reg_string = r".*(" + "|".join(regex_extensions) + r")"
+        REGEX = [reg_string]
+        print('RegEx:', REGEX)
+
+        src_path = sys.argv[4] if len(sys.argv) > 1 else '.'
+        print('Watch Path:', src_path)
+        inp = input('Press y if configurations are okay: ')
+        if inp.strip().lower() == 'y':
+            FileWatcher(src_path, REGEX, sub_folders, BUFFER_TIME).run()
+        else:
+            return print('Try again.')
+
+
+    execute()
